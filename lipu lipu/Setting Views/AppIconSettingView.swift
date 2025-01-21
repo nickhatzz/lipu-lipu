4e8562304b80ea0ba7a437ab4389e80be5fb5905
738991 <738991@sylvaniastudents.org>
2025-01-21 17:12:39 -0500

    adding settings to CreditsView (not functional yet); other minor changes

---
+++ lipu lipu/Setting Views/AppIconSettingView.swift
@@ -0,0 +1,18 @@
+//
+//  AppIconSettingView.swift
+//  lipu lipu
+//
+//  Created by Nick on 1/21/25.
+//
+
+import SwiftUI
+
+struct AppIconSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppIconSettingView()
+}
---
+++ lipu lipu/Setting Views/DisplaySettingView.swift
@@ -0,0 +1,18 @@
+//
+//  DisplaySettingView.swift
+//  lipu lipu
+//
+//  Created by Nick on 1/21/25.
+//
+
+import SwiftUI
+
+struct DisplaySettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DisplaySettingView()
+}
