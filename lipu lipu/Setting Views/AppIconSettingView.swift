6ff26139745ad75687ef1d06b0628a7a937e8d89
Nick <104640455+738991@users.noreply.github.com>
2025-02-26 14:16:08 -0500

    worked on AppIconSettingView

--- lipu lipu/Setting Views/AppIconSettingView.swift
+++ lipu lipu/Setting Views/AppIconSettingView.swift
@@ -8,27 +8,40 @@
 import SwiftUI
 
 struct AppIconSettingView: View {
-    var icons: [String] = ["logo", "2", "3", "4", "5", "6"]
+    var icons: [String] = ["AppIcon1", "AppIcon2", "AppIcon3", "AppIcon4", "AppIcon5"]
     let adaptiveColumn = [
-        GridItem(.fixed(100)),
-        GridItem(.fixed(100)),
-        GridItem(.fixed(100))
+        GridItem(.fixed(110)),
+        GridItem(.fixed(110)),
+        GridItem(.fixed(110))
     ]
     
     var body: some View {
         NavigationStack {
             ScrollView {
                 LazyVGrid(columns: adaptiveColumn, spacing: 10) {
+                    // default icon
+                    Button {
+                        UIApplication.shared.setAlternateIconName(nil)
+                    } label: {
+                        VStack {
+                            Image("AppIconImage")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .cornerRadius(15)
+                        }
+                    }
+                    // other icons
                     ForEach(icons, id: \.self) { icon in
                         Button {
                             UIApplication.shared.setAlternateIconName(icon)
                         } label: {
                             VStack {
-                                Image("logo")
+                                Image("\(icon)Image")
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 100, height: 100)
-                                Text(icon)
+                                    .cornerRadius(15)
                             }
                         }
                     }
