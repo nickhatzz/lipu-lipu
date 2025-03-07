ccffead0260d94a19f740fda9a5d4cb0bb8c8ab3
Nick <104640455+738991@users.noreply.github.com>
2025-03-07 14:25:12 -0500

    started making words load from lipu Linku api instead of text file

---
+++ lipu lipu/Helper Classes/WordViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  WordViewModel.swift
+//  lipu lipu
+//
+//  Created by HPro2 on 3/7/25.
+//
+
+import Foundation
+
+class apiCall {
+    func getWords(completion:@escaping ([String: Word]) -> ()) {
+        guard let url = URL(string: "https://api.linku.la/v1/words") else { return }
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            let words = try! JSONDecoder().decode([String: Word].self, from: data!)
+            print(words)
+            
+            DispatchQueue.main.async {
+                completion(words)
+            }
+        }
+        .resume()
+    }
+}
--- lipu lipu/Main Views/DictionaryView.swift
+++ lipu lipu/Main Views/DictionaryView.swift
@@ -10,18 +10,29 @@ import SwiftUI
 struct DictionaryView: View {
     @State private var searchText = ""
     var words: [[String]] = getWords() // word[0] = word; word[1] = translations; word[2] = source
-        
+    @State var apiWords: [String: Word] = [:]
+    
     var body: some View {
         
         NavigationStack {
             List {
-                ForEach(searchResults, id: \.self) { word in
-                    NavigationLink(destination: WordView(word: word[0], translations: word[1], source: word[2])) {
-                        WordPreviewCellView(word: word[0], translations: word[1], source: word[2])
+//                ForEach(searchResults, id: \.self) { word in
+//                    NavigationLink(destination: WordView(word: word[0], translations: word[1], source: word[2])) {
+//                        WordPreviewCellView(word: word[0], translations: word[1], source: word[2])
+//                    }
+//                }
+                ForEach(apiWords.keys.sorted(by: <), id: \.self) { key in
+                    NavigationLink(destination: WordView(word: apiWords[key]!.word, translations: apiWords[key]!.translations["en"]!.definition, source: apiWords[key]!.word)) {
+                        WordPreviewCellView(word: apiWords[key]!.word, translations: apiWords[key]!.translations["en"]!.definition, source: apiWords[key]!.word)
                     }
                 }
             }
             .navigationTitle("Dictionary")
+            .onAppear {
+                apiCall().getWords { (words) in
+                    self.apiWords = words
+                }
+            }
         }
         .searchable(text: $searchText)
 
